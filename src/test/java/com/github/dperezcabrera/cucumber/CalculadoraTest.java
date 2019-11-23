4f10029da88faddcb5e31cb519dee7bc49e9127e
dperezcabrera <dperezcabrera@gmail.com>
2019-11-23 18:48:10 +0100

    Actualizar version de cucumber

--- src/test/java/com/github/dperezcabrera/cucumber/CalculadoraTest.java
+++ src/test/java/com/github/dperezcabrera/cucumber/CalculadoraTest.java
@@ -1,7 +1,7 @@
 package com.github.dperezcabrera.cucumber;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @CucumberOptions(features = "src/test/resources/features/calculadora.feature", plugin = {"pretty", "html:target/cucumber/calculadora"})
