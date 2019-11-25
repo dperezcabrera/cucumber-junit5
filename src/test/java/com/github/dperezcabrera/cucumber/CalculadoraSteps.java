fe7a172f1c7e427c94d0b5066919979d47b87bbb
dperezcabrera <dperezcabrera@gmail.com>
2019-11-25 15:53:30 +0100

    Refactorizacion test

--- src/test/java/com/github/dperezcabrera/cucumber/CalculadoraSteps.java
+++ src/test/java/com/github/dperezcabrera/cucumber/CalculadoraSteps.java
@@ -11,17 +11,17 @@ public class CalculadoraSteps {
     private Calculadora calculadora;
     private int resultado;
 
-    @Given("^una calculadora")
+    @Given("una calculadora")
     public void una_calculadora() throws Throwable {
         calculadora = new Calculadora();
     }
 
-    @When("^realizamos la suma entre (-?\\d+) y (-?\\d+)$")
+    @When("realizamos la suma entre {int} y {int}")
     public void realizamos_la_suma_entre_dos_numero(int arg1, int arg2) throws Throwable {
         resultado = calculadora.suma(arg1, arg2);
     }
 
-    @Then("^el resultado esperado es (-?\\d+)$")
+    @Then("el resultado esperado es {int}")
     public void el_resultado_esperado_es(int resultadoEsperado) throws Throwable {
         assertEquals(resultado, resultadoEsperado);
     }
